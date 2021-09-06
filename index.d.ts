8a679ceb37c127bf2e462969a82c0b7b623480b0
tony-go <gorez.tony@gmail.com>
2021-09-06 22:04:16 +0200

    chore: enrich index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,47 @@
 // This represents the JavaScript class which would be available at runtime
 export default class PluginFactory {
   constructor(options: PluginFactory.Options);
+
+  /**
+   * Plugin name, could be use as default tag
+   */
+  name: string;
+
+  /**
+   * add a Given step
+   */
+  addGivenStep: PluginFactory.AddStepFunction;
+
+  /**
+   * add a When step
+   */
+  addWhenStep: PluginFactory.AddStepFunction;
+
+  /**
+   * add a Then step
+   */
+  addThenStep: PluginFactory.AddStepFunction;
+
+  /**
+   * Will make all steps available int RestQA
+   */
+  ready(): void
 }
-// This namespace is merged with the API class and allows for consumers, and this file
-// to have types which are nested away in their own sections.
+
 declare namespace PluginFactory {
+  export type Definition = string;
+  export type HandlerFunc = (...args: any) => void;
+  export type Description = string;
+  export type Tags = string | string [];
+
+  export type AddStepFunction = (
+    stepDefinition: Definition, 
+    handler: HandlerFunc,
+    description: Description,
+    tags?: Tags
+  ) => void
+
+  export type Step = [Definition, HandlerFunc, Description] | [Definition, HandlerFunc, Description, Tags]
   export interface Options {
     name: string
   }
