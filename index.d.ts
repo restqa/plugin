e79994b9656fe347016837710813cc17d62a4eee
tony-go <gorez.tony@gmail.com>
2021-09-09 09:16:47 +0200

    feat: enrich types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,20 @@
+import {
+  After,
+  AfterAll,
+  AfterStep,
+  Before,
+  BeforeAll,
+  BeforeStep,
+  defineParameterType,
+  defineStep,
+  Given,
+  setDefaultTimeout,
+  setDefinitionFunctionWrapper,
+  setWorldConstructor,
+  Then,
+  When,
+} from "@cucumber/cucumber/lib/types"
+
 // This represents the JavaScript class which would be available at runtime
 export default class PluginFactory {
   constructor(options: PluginFactory.Options);
@@ -23,12 +40,37 @@ export default class PluginFactory {
   addThenStep: PluginFactory.AddStepFunction;
 
   /**
-   * Will make all steps available int RestQA
+   * add a state property
    */
-  ready(): void
+  addState: <T>(key: string, value: T) => void;
+
+  /**
+   * add a state property
+   */
+  addBeforeHook: AddHookFunc;
+
+  /**
+   * add a state property
+   */
+  addAfterHook: AddHookFunc;
+
+  /**
+   * Will make all steps available in RestQA
+   * And return an Export Object
+   */
+  private _apply(
+    cucumberInstance: PluginFactory.Cucumber,
+    config: Record<string, any>
+  ): PluginFactory.ExportObject;
 }
 
 declare namespace PluginFactory {
+  export interface Options {
+    name: string;
+  }
+
+  export type AddHookFunc = (...args: any) => void;
+
   export type Definition = string;
   export type HandlerFunc = (...args: any) => void;
   export type Description = string;
@@ -38,11 +80,47 @@ declare namespace PluginFactory {
     stepDefinition: Definition, 
     handler: HandlerFunc,
     description: Description,
-    tags?: Tags
+    tags?: Tag
   ) => void
 
-  export type Step = [Definition, HandlerFunc, Description] | [Definition, HandlerFunc, Description, Tags]
-  export interface Options {
-    name: string
+  export interface Step {
+    step: Definition;
+    fn: HandlerFunc;
+    description: Description;
+    tags?: Tags;
+  }
+
+  export type States = Record<string, any>;
+
+  export type Hook = (...args: any[]) => void
+
+  export interface ExportObject {
+    states: States;
+    steps: {
+      given: Step[];
+      when: Step[];
+      then: Step[];
+    };
+    hooks: {
+      before: Hook[];
+      after: Hook[];
+    };
+  }
+
+  export interface Cucumber {
+    After: After;
+    AfterAll: AfterAll;
+    AfterStep: AfterStep;
+    Before: Before;
+    BeforeAll: BeforeAll;
+    BeforeStep: BeforeStep;
+    defineParameterType: defineParameterType;
+    defineStep: defineStep;
+    Given: Given;
+    setDefaultTimeout: setDefaultTimeout;
+    setDefinitionFunctionWrapper: setDefinitionFunctionWrapper;
+    setWorldConstructor: setWorldConstructor;
+    Then: Then;
+    When: When;
   }
 }
